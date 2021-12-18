53f9ed667f074eb8ebdd7c25b5153f554bb2c751
Dmitriy <shaytan_truba_xd@mail.ru>
2021-12-18 18:10:51 +0200

    save project on PC

--- python/SpaceObjects/Mobs/MobsBase.py
+++ python/SpaceObjects/Mobs/MobsBase.py
@@ -1,12 +1,15 @@
+import time
+
 from ...Packages.ShipState import ShipState
+from ...MyUtils.ThreadBase import ThreadBase
 
 
-class Mobs(ShipState):
+class Mobs(ShipState, ThreadBase):
     class_number: int = 0
     __variable: list = ["player", "mob", "object", ""]
 
     def __init__(self, dict_: dict):
-        self.__toBegin()
+        self.__to_begin()
 
         self.device: list = dict_["device"]
         self.weapons: list = dict_["weapons"]
@@ -22,27 +25,23 @@ class Mobs(ShipState):
         self.health = dict_["health"]
         self.energy = dict_["energy"]
         self.speed = dict_["speed"]
+        self.repairHealth = dict_["repairHealth"]  # восстановление хп
+        self.repairEnergy = dict_["repairEnergy"]  # восстановление энки
+        self.max_health = self.health
+        self.max_energy = self.energy
 
     def main(self):
-        objects = self.requestLocation()
+        self.start_update("__all_repair", 1)
+        self.start_update("start", 5)
+
+    def start(self):
+        objects = self.request_location()
         if not self.__isTaking and not self.__isAttack:
             for obj in objects:
                 self.send_dev(obj)
         else:
             if self.object_to_reach_id not in objects:
-                self.__toBegin()
-
-    def update(self):
-        self.move()
-
-    def move(self):
-        pass
-
-    def requestLocation(self):
-        # запрос к локации, ответ в переменную response
-        response = None
-        objects = response.object  # все объекты на локации
-        return objects
+                self.__to_begin()
 
     def send_dev(self, object=None):
         typeObject: str = self.__examination(object)
@@ -53,19 +52,27 @@ class Mobs(ShipState):
         elif typeObject == self.__variable[2]:
             self.__take(object.guid)
 
-    def getPlayerDamage(self, id: int, hp: int) -> None:
-        self.__setHealth(hp)
+    def update(self):
+        self.move()
+
+    def move(self):
+        pass
+
+    def get_player_damage(self, id: int, hp: int) -> None:
+        self.__set_health(hp)
         self.__attack(id)
 
-    def getDevice(self) -> list:
+    def get_device(self) -> list:
         return self.device
 
-    def getWeapons(self) -> list:
+    def get_weapons(self) -> list:
         return self.weapons
 
-    def __toBegin(self):
-        self.__isTaking: bool = False
-        self.__isAttack: bool = False
+    def request_location(self):
+        # запрос к локации, ответ в переменную response
+        response = None
+        objects = response.object  # все объекты на локации
+        return objects
 
     def __examination(self, object) -> str:
         if object.id > 0:
@@ -76,6 +83,21 @@ class Mobs(ShipState):
             return self.__variable[2]
         else:
             return self.__variable[3]
+
+    def __all_repair(self):
+        self.health = self.__repair(self.health, self.max_health, self.repairHealth)
+        self.energy = self.__repair(self.energy, self.max_energy, self.repairEnergy)
+
+    def __repair(self, param, max_params, repair):
+        if param < max_params:
+            param += repair
+        else:
+            param = max_params
+        return param
+
+    def __to_begin(self):
+        self.__isTaking: bool = False
+        self.__isAttack: bool = False
 
     def __attack(self, id: int):
         self.__isTaking = False
@@ -88,8 +110,8 @@ class Mobs(ShipState):
         self.object_to_reach_id = guid
         pass
 
-    def __setHealth(self, health: int):
+    def __set_health(self, health: int):
         self.health -= health
 
-    def __setEnergy(self, energy: int):
+    def __set_energy(self, energy: int):
         self.energy -= energy
